f565413cc1b734e7df7743d6d64220d083cd0450
Mehdi Salimi <mahdisalimi02@gmail.com>
2022-03-19 09:59:17 +0330

    Added factory and seeders

--- database/seeders/CostSeeder.php
+++ database/seeders/CostSeeder.php
@@ -9,11 +9,13 @@ use App\Models\Cost;
 
 class CostSeeder extends Seeder
 {
-    
+    /*
+     * create 10 fake costs.
+     */
     public function run()
     {
         $count = 10;
-        while($count>10){
+        while($count > 1){
             $count--;
             Cost::firstOrCreate([
                 'cost_category_id' => rand(1,6),
